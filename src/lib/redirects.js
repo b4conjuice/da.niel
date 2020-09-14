275fe8e1b9a1a5b8d40c1b5eb101cc883d1dc684
daniel lopez <dlopez807@gmail.com>
2020-09-14 16:55:05 -0700

    fix mobile styles

--- src/lib/redirects.js
+++ src/lib/redirects.js
@@ -1,6 +1,5 @@
 const redirects = {
   next: 'https://nextjs.org',
-  redirect: 'https://nextjs.org/docs/api-reference/next.config.js/redirects',
   tailwind: 'https://tailwindcss.com',
   vercel: 'https://vercel.com',
   bacon: 'https://with-bacon.niel.to',
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,25 +7,34 @@ import redirects from '@/lib/redirects'
 const Home = () => (
   <Page>
     <main className="flex flex-col justify-center flex-grow space-y-4">
-      <Title>da.niel</Title>
-      <p className="text-2xl text-center text-cobalt sm:text-3xl">
-        url shortener using{' '}
+      <div className="flex justify-center space-x-4">
+        <Title>da.niel</Title>
         <a
-          className="font-semibold hover:underline"
-          href="https://nextjs.org"
+          className="inline-flex p-1 text-gray-100 rounded-full bg-cobalt hover:bg-cb-dusty-blue"
+          href="https://github.com/b4conjuice/da.niel"
           target="_blank"
           rel="noopener noreferrer"
         >
-          next.js
-        </a>{' '}
+          <svg
+            className="w-8 h-8 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <title>GitHub</title>
+            <path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0" />
+          </svg>
+        </a>
+      </div>
+      <p className="px-4 text-2xl text-center text-cobalt sm:text-3xl">
+        url shortener using <br className="block md:hidden" />
         <a
           className="font-semibold hover:underline"
           href="https://nextjs.org/docs/api-reference/next.config.js/redirects"
           target="_blank"
           rel="noopener noreferrer"
         >
-          redirects
-        </a>
+          next.js redirects
+        </a>{' '}
       </p>
       <ul className="space-y-2 text-xl">
         {Object.entries(redirects).map(([key, value]) => (

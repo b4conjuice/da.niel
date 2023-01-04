a713d9274ada4060f49eb4efc9490936fdfad5dd
Daniel Lopez <dlopez807@gmail.com>
2023-01-03 16:13:28 -0800

    update redirects

--- src/lib/redirects.js
+++ src/lib/redirects.js
@@ -2,7 +2,7 @@ const redirects = {
   next: 'https://nextjs.org',
   tailwind: 'https://tailwindcss.com',
   vercel: 'https://vercel.com',
-  bacon: 'https://with-bacon.niel.to',
+  bacon: 'https://with-bacon.dlopez.app',
 }
 
 module.exports = redirects
